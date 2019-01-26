a817f99dc278b3027a656db13161e98069dd6413
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-01-26 01:41:45 -0500

    Implement ToString for TenantData

--- Assets/Code/Tenant.cs
+++ Assets/Code/Tenant.cs
@@ -8,12 +8,18 @@ namespace Gameplay {
 
     [System.Serializable]
     public class TenantData {
+        // Note: class is hardcoded for only a single trait
         public List<Trait> traits;
 
-        // Currently hardcoded for only a single trait
         public TenantData(Trait trait) {
             traits = new List<Trait>();
             traits.Add(trait);
+        }
+
+        public override string ToString() {
+            var trait = traits[0];
+            return (trait.like ? "Likes" : "Hates")
+                + " " + trait.data.title;
         }
     }
 }
