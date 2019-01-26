c2ce181664ba9a6833846e128a7a7e9d80bf5641
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-01-26 00:51:54 -0500

    Implement preliminary trait

---
+++ Assets/Code/Trait.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Gameplay {
+    [System.Serializable]
+    public class Trait {
+        public TraitData data;
+        public bool like;
+
+        public Trait(TraitData data, bool like) {
+            this.data = data;
+            this.like = like;
+        }
+    }
+
+    [System.Serializable]
+    public class TraitData {
+        public string title;
+    }
+}
